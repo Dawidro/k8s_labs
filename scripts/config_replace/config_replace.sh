36aadb3ad3cef3143a43c58a27d98b0d46a76b94
Dawidro <dawidro@gmail.com>
2022-12-18 19:52:34 +0000

    fixes

--- scripts/config_replace/config_replace.sh
+++ scripts/config_replace/config_replace.sh
@@ -5,9 +5,9 @@ n=1
 while IFS= read -r "VAR$n"; do
   n=$((n + 1))
 done < config.txt
-find ../../[0-9]* -type f -exec sed -i -e 's/example.com/${var1}/g' {} \;
-find ../../[0-9]* -type f -exec sed -i -e 's/myemail@mail.com/${var2}/g' {} \;
-find ../../[0-9]* -type f -exec sed -i -e 's/192.168.0.1/${var3}/g' {} \;
+find ../../[0-9]* -type f -exec sed -i -e "s/example.com/${VAR1}/g" {} \;
+find ../../[0-9]* -type f -exec sed -i -e "s/myemail@mail.com/${VAR2}/g" {} \;
+find ../../[0-9]* -type f -exec sed -i -e "s/192.168.0.1/${VAR3}/g" {} \;
 echo "${VAR1}"
 echo "${VAR2}"
 echo "${VAR3}"
--- scripts/iptables_scripts/iptables_chains.sh
+++ scripts/iptables_scripts/iptables_chains.sh
@@ -2,6 +2,6 @@
 IPTABLES=$(iptables -S)
 IPTABLES=$(sed -e "s/^/iptables /" <<< "${IPTABLES}")
 NATTABLES=$(iptables -t nat -S)
-NATTABLES=$(sed -e "s/^/iptables /" <<< "${NATTABLES}")
+NATTABLES=$(sed -e "s/^/iptables -t nat /" <<< "${NATTABLES}")
 echo "${IPTABLES}" > iptables.sh
 echo "${NATTABLES}" > nat.sh
